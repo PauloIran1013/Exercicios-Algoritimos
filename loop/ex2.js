499708f7dcb3892844cd7b6daa52b287af9b8c6c
PauloIran1013 <julioirancoelho@gmail.com>
2024-09-11 17:10:46 -0300

    dicionei loop2

--- loop/ex2.js
+++ loop/ex2.js
@@ -1,6 +1,6 @@
-for (let i = 1; i<=4; i++){
+for (let i = 1; i<=10; i++){
     let row=""
-    for (let k = 1; k <= 4 - i; k++) {
+    for (let k = 1; k <= 10 - i; k++) {
         row += " ";
     }
     for(let j=1;j<=i;j++){
