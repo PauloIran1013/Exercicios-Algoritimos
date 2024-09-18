ef3dd036128b13fe365987526508119258943237
PauloIran1013 <julioirancoelho@gmail.com>
2024-09-18 13:38:55 -0300

    sla

--- array2/ex7.js
+++ array2/ex7.js
@@ -2,6 +2,7 @@ const prompt = require('prompt-sync')()
 const n = prompt("Digite números separados por vírgula (ex: 1,2,3,4,5):")
 const vetor = n.split(',').map(Number)  
 
+let sla = vetor[0]
 let soma = 0
 
 vetor.forEach(num => soma +=num)
